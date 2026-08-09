4e85836ac53a953297bb192233533cbd1eca3908
Vaibhavi Sawant <96656695+vaibhaviksawant@users.noreply.github.com>
2026-08-09 20:51:28 +0530

    Create Solution.java
    added

---
+++ 509-Fibonacci-Number/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int fib(int n) {
+        if(n < 2)
+            return n;
+        int[] fib = new int[n+1];
+        fib[0] = 0;
+        fib[1] = 1;
+        for(int i = 2; i < n+1; i++) {
+            fib[i] = fib[i-1]+fib[i-2];
+        }
+
+        return fib[n];
+    }
+}
